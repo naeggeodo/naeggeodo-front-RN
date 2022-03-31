d9f6742f55d5f8aa4e46f16d445f8f3819f8b54e
JYCHO <cjy0029@naver.com>
2022-03-31 13:58:14 +0900

    feat : 메인 메뉴 슬라이드 ui 구현

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import SlideCategory from './src/components/main/SlideCategory';
 import LoginPage from './src/pages/LoginPage';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <LoginPage />
+      <SlideCategory />
     </SafeAreaView>
   );
 };
---
+++ src/data/mainMockDatas.ts
@@ -0,0 +1,11 @@
+// ** 메인페이지 카테고리 목데이터 **
+export const categoryMockData = [
+  {id: 1, name: '전체'},
+  {id: 2, name: '치킨'},
+  {id: 3, name: '버거'},
+  {id: 4, name: '한식'},
+  {id: 5, name: '일식/돈까스'},
+  {id: 6, name: '카페/디저트'},
+  {id: 7, name: '족발/보쌈'},
+  {id: 8, name: '고기/구이'},
+];
