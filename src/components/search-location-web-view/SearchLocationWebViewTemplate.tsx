f2e9d941d051ba4da4c3409dd338102c349f5a24
JYCHO <cjy0029@naver.com>
2022-04-06 20:10:47 +0900

    feat : 주소 등록 팝업 가운데 정렬 및 스타일링 완료

--- src/components/search-location-web-view/SearchLocationWebViewTemplate.tsx
+++ src/components/search-location-web-view/SearchLocationWebViewTemplate.tsx
@@ -4,23 +4,43 @@ import Postcode from '@actbase/react-daum-postcode';
 
 const SearchLocationWebViewTemplate = () => {
   return (
-    <View style={styles.webviewContainer}>
-      <Postcode
-        onError={() => {}}
-        style={{width: 400, height: 320}}
-        jsOptions={{animation: true}}
-        onSelected={data => Alert.alert(JSON.stringify(data))}
-      />
-    </View>
+    <>
+      <View style={styles.backgroundBlack}></View>
+      <View style={styles.webviewContainer}>
+        <Postcode
+          onError={() => {}}
+          style={{width: 350, height: 500, flex: 1}}
+          jsOptions={{animation: true}}
+          onSelected={data => Alert.alert(JSON.stringify(data))}
+        />
+      </View>
+    </>
   );
 };
 
 export default SearchLocationWebViewTemplate;
 
 const styles = StyleSheet.create({
-  webviewContainer: {
-    zIndex: 2,
+  backgroundBlack: {
+    position: 'absolute',
+    alignItems: 'center',
     top: 0,
     left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 10,
+    backgroundColor: '#000000',
+    opacity: 0.6,
+  },
+
+  webviewContainer: {
+    position: 'absolute',
+    zIndex: 11,
+    top: '10%',
+    left: 0,
+    right: 0,
+    bottom: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
