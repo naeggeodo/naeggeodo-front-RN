38f3bd555791ef33e3e6831f4f87ffd25b250d5c
JYCHO <cjy0029@naver.com>
2022-04-07 23:45:33 +0900

    feat : 내꺼도 메인 탭 메뉴 모듈화

--- App.tsx
+++ App.tsx
@@ -4,30 +4,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {useState} from 'react';
 // pages import
-import ChatRoomPage from './src/pages/ChatRoomPage';
 import LoginPage from './src/pages/LoginPage';
-import MainPage from './src/pages/MainPage';
-import MorePage from './src/pages/MorePage';
-import OrderPage from './src/pages/OrderPage';
-import SearchPage from './src/pages/SearchPage';
-// icons import
-import HomeBlack from './src/assets/icons/homeblack.svg';
-import HomeGray from './src/assets/icons/homegray.svg';
-import SearchGray from './src/assets/icons/searchgray.svg';
-import SearchBlack from './src/assets/icons/searchblack.svg';
-import PlusGray from './src/assets/icons/plusgray.svg';
-import PlusBlack from './src/assets/icons/plusblack.svg';
-import ChatGray from './src/assets/icons/chatgray.svg';
-import ChatBlack from './src/assets/icons/chatblack.svg';
-import MoreGray from './src/assets/icons/moregray.svg';
-import MoreBlack from './src/assets/icons/moreblack.svg';
-import fonts from './src/styles/fonts';
-import palette from './src/styles/palette';
+import NaggeodoTabNavigator from './src/components/Navigator/NaggeodoTabNavigator';
+
 import {Provider} from 'react-redux';
 import store from './src/store';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export type LoggedInParamList = {
@@ -49,69 +32,7 @@ function App() {
       <SafeAreaProvider>
         <NavigationContainer>
           {isLoggedIn ? (
-            <Tab.Navigator
-              initialRouteName="Home"
-              screenOptions={{
-                tabBarStyle: {
-                  borderTopStartRadius: 20,
-                  borderTopEndRadius: 20,
-                  borderColor: `${palette.LineGray}`,
-                  backgroundColor: 'white',
-                },
-                headerShown: false,
-                tabBarActiveTintColor: `${palette.black}`,
-                tabBarInactiveTintColor: `${palette.LineGray}`,
-                tabBarLabelStyle: {
-                  fontFamily: `${fonts.sqRegular}`,
-                  fontSize: 11,
-                },
-              }}>
-              <Tab.Screen
-                name="Home"
-                component={MainPage}
-                options={{
-                  title: '홈',
-                  tabBarIcon: ({focused}) =>
-                    focused ? <HomeBlack /> : <HomeGray />,
-                }}
-              />
-              <Tab.Screen
-                name="Search"
-                component={SearchPage}
-                options={{
-                  title: '검색',
-                  tabBarIcon: ({focused}) =>
-                    focused ? <SearchBlack /> : <SearchGray />,
-                }}
-              />
-              <Tab.Screen
-                name="Orders"
-                component={OrderPage}
-                options={{
-                  title: '내꺼도 생성',
-                  tabBarIcon: ({focused}) =>
-                    focused ? <PlusBlack /> : <PlusGray />,
-                }}
-              />
-              <Tab.Screen
-                name="Chattings"
-                component={ChatRoomPage}
-                options={{
-                  title: '내꺼도',
-                  tabBarIcon: ({focused}) =>
-                    focused ? <ChatBlack /> : <ChatGray />,
-                }}
-              />
-              <Tab.Screen
-                name="More"
-                component={MorePage}
-                options={{
-                  title: '전체',
-                  tabBarIcon: ({focused}) =>
-                    focused ? <MoreBlack /> : <MoreGray />,
-                }}
-              />
-            </Tab.Navigator>
+            <NaggeodoTabNavigator />
           ) : (
             <Stack.Navigator>
               <Stack.Screen
---
+++ src/components/Navigator/NaggeodoTabNavigator.tsx
@@ -0,0 +1,101 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+
+import HomeBlack from '../../assets/icons/homeblack.svg';
+import HomeGray from '../../assets/icons/homegray.svg';
+import SearchGray from '../../assets/icons/searchgray.svg';
+import SearchBlack from '../../assets/icons/searchblack.svg';
+import PlusGray from '../../assets/icons/plusgray.svg';
+import PlusBlack from '../../assets/icons/plusblack.svg';
+import ChatGray from '../../assets/icons/chatgray.svg';
+import ChatBlack from '../../assets/icons/chatblack.svg';
+import MoreGray from '../../assets/icons/moregray.svg';
+import MoreBlack from '../../assets/icons/moreblack.svg';
+import ChatRoomPage from '../../pages/ChatRoomPage';
+import MainPage from '../../pages/MainPage';
+import MorePage from '../../pages/MorePage';
+import OrderPage from '../../pages/OrderPage';
+import SearchPage from '../../pages/SearchPage';
+import fonts from '../../styles/fonts';
+import palette from '../../styles/palette';
+import {Platform} from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+const NaggeodoTabNavigator = () => {
+  return (
+    <>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarStyle: {
+            borderTopWidth: 1,
+            borderRightWidth: 1,
+            borderLeftWidth: 1,
+            borderColor: `${palette.LineGray}`,
+            borderTopStartRadius: 20,
+            borderTopEndRadius: 20,
+            position: 'absolute',
+            overflow: 'hidden',
+            left: 0,
+            bottom: 0,
+            right: 0,
+            padding: 5,
+            height: 85,
+          },
+
+          headerShown: false,
+          tabBarActiveTintColor: `${palette.black}`,
+          tabBarInactiveTintColor: `${palette.LineGray}`,
+          tabBarLabelStyle: {
+            fontFamily: `${fonts.sqRegular}`,
+            fontSize: 11,
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={MainPage}
+          options={{
+            title: '홈',
+            tabBarIcon: ({focused}) => (focused ? <HomeBlack /> : <HomeGray />),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchPage}
+          options={{
+            title: '검색',
+            tabBarIcon: ({focused}) =>
+              focused ? <SearchBlack /> : <SearchGray />,
+          }}
+        />
+        <Tab.Screen
+          name="Orders"
+          component={OrderPage}
+          options={{
+            title: '내꺼도 생성',
+            tabBarIcon: ({focused}) => (focused ? <PlusBlack /> : <PlusGray />),
+          }}
+        />
+        <Tab.Screen
+          name="Chattings"
+          component={ChatRoomPage}
+          options={{
+            title: '내꺼도',
+            tabBarIcon: ({focused}) => (focused ? <ChatBlack /> : <ChatGray />),
+          }}
+        />
+        <Tab.Screen
+          name="More"
+          component={MorePage}
+          options={{
+            title: '전체',
+            tabBarIcon: ({focused}) => (focused ? <MoreBlack /> : <MoreGray />),
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  );
+};
+
+export default NaggeodoTabNavigator;
