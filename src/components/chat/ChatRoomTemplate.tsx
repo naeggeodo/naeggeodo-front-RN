3af97e8366a36b653e142a3046ed952134980fc4
JYCHO <cjy0029@naver.com>
2022-05-11 16:56:10 +0900

    feat : 가게정보 보러가기 버튼 생성

--- src/components/chat/ChatRoomTemplate.tsx
+++ src/components/chat/ChatRoomTemplate.tsx
@@ -1,8 +1,9 @@
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Pressable, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import ChatRoomInfo from './ChatRoomInfo';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import palette from '../../styles/palette';
+import ArrowRight from '../../assets/icons/arrow_right.svg';
 
 const ChatRoomTemplate = ({navigation}: {navigation: any}) => {
   return (
@@ -10,6 +11,15 @@ const ChatRoomTemplate = ({navigation}: {navigation: any}) => {
       <StatusBar barStyle="light-content" />
       <View style={styles.template}>
         <ChatRoomInfo navigation={navigation} />
+
+        <View style={styles.chattingContainer}>
+          <View style={{alignItems: 'center'}}>
+            <Pressable style={styles.storeInfoButton}>
+              <Text>가게정보 보러가기</Text>
+              <ArrowRight />
+            </Pressable>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -19,6 +29,29 @@ const styles = StyleSheet.create({
   template: {
     height: '100%',
     backgroundColor: '#ffffff',
+  },
+  chattingContainer: {
+    flex: 1,
+    backgroundColor: `${palette.bgGray}`,
+  },
+  storeInfoButton: {
+    width: '90%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: '#ffffff',
+    marginTop: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
   },
 });
 
